13ae0ac9f0fb49ca9f48e966b1f0f8464a51a733
zach beecher <zachbeecher@gmail.com>
2017-11-19 19:58:48 -0800

    add routing and params attributes, private

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
     def index
-
+        @items = Item.all.order("created_at DESC")
     end
 
     def new
@@ -9,6 +9,17 @@ class ItemsController < ApplicationController
     end
 
     def create
-        
+        @item = Item.new(item_params)
+        if @item.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+    private 
+
+    def item_params
+        params.require(:item).permit(:title, :description)
     end
 end
